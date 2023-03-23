35677a29e32094ef6435a7a60d637765ac7faabc
tami <xorud7979@gmail.com>
2023-03-23 11:24:34 +0900

    feat: try use Chakra UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import { ChakraProvider } from "@chakra-ui/react";
 import PrintToOut from "./components/PrintToOut";
 
 function App() {
   return (
-    <>
+    <ChakraProvider>
       <PrintToOut />
-    </>
+    </ChakraProvider>
   );
 }
 
--- src/components/PrintToOut.js
+++ src/components/PrintToOut.js
@@ -1,5 +1,26 @@
+import { Button, Container, Flex } from "@chakra-ui/react";
 import React, { useRef } from "react";
 import { useReactToPrint } from "react-to-print";
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tfoot,
+  Tr,
+  Th,
+  Td,
+  TableContainer,
+} from "@chakra-ui/react";
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  Heading,
+  Stack,
+  StackDivider,
+  Box,
+  Text,
+} from "@chakra-ui/react";
 
 const PrintToOut = () => {
   const componentRef = useRef();
@@ -9,36 +30,85 @@ const PrintToOut = () => {
 
   return (
     <>
-      <div>
-        <button onClick={handlePrint}>Print this out!</button>
-      </div>
-      <div ref={componentRef}>
-        <h2>Attendance</h2>
-        <table>
-          <thead>
-            <th>S/N</th>
-            <th>Name</th>
-            <th>Email</th>
-          </thead>
-          <tbody>
-            <tr>
-              <td>1</td>
-              <td>Njoku Samson</td>
-              <td>samson@yahoo.com</td>
-            </tr>
-            <tr>
-              <td>2</td>
-              <td>Ebere Plenty</td>
-              <td>ebere@gmail.com</td>
-            </tr>
-            <tr>
-              <td>3</td>
-              <td>Undefined</td>
-              <td>No Email</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+      <Container p={10}>
+        <Flex justify={"center"} paddingBottom={5}>
+          <Button onClick={handlePrint} colorScheme="teal">
+            Print this out!
+          </Button>
+        </Flex>
+
+        <div ref={componentRef}>
+          <TableContainer paddingBottom={10}>
+            <Table variant="simple">
+              <Thead>
+                <Tr>
+                  <Th>To convert</Th>
+                  <Th>into</Th>
+                  <Th isNumeric>multiply by</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                <Tr>
+                  <Td>inches</Td>
+                  <Td>millimetres (mm)</Td>
+                  <Td isNumeric>25.4</Td>
+                </Tr>
+                <Tr>
+                  <Td>feet</Td>
+                  <Td>centimetres (cm)</Td>
+                  <Td isNumeric>30.48</Td>
+                </Tr>
+                <Tr>
+                  <Td>yards</Td>
+                  <Td>metres (m)</Td>
+                  <Td isNumeric>0.91444</Td>
+                </Tr>
+              </Tbody>
+              <Tfoot>
+                <Tr>
+                  <Th>To convert</Th>
+                  <Th>into</Th>
+                  <Th isNumeric>multiply by</Th>
+                </Tr>
+              </Tfoot>
+            </Table>
+          </TableContainer>
+
+          <Card variant={"filled"}>
+            <CardHeader>
+              <Heading size="md">Client Report</Heading>
+            </CardHeader>
+            <CardBody>
+              <Stack divider={<StackDivider />} spacing="4">
+                <Box>
+                  <Heading size="xs" textTransform="uppercase">
+                    Summary
+                  </Heading>
+                  <Text pt="2" fontSize="sm">
+                    View a summary of all your clients over the last month.
+                  </Text>
+                </Box>
+                <Box>
+                  <Heading size="xs" textTransform="uppercase">
+                    Overview
+                  </Heading>
+                  <Text pt="2" fontSize="sm">
+                    Check out the overview of your clients.
+                  </Text>
+                </Box>
+                <Box>
+                  <Heading size="xs" textTransform="uppercase">
+                    Analysis
+                  </Heading>
+                  <Text pt="2" fontSize="sm">
+                    See a detailed analysis of all your business clients.
+                  </Text>
+                </Box>
+              </Stack>
+            </CardBody>
+          </Card>
+        </div>
+      </Container>
     </>
   );
 };
